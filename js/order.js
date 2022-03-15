a1dda325720e0e44cd9a89f6689dab9d69c929c3
sergey-popov-1964 <73323891+sergey-popov-1964@users.noreply.github.com>
2022-03-15 16:42:12 +0700

    15_03_2022__16_42

--- js/order.js
+++ js/order.js
@@ -1,28 +1,28 @@
-let modal = document.querySelector('.modal');
-let buttonOrder = document.querySelectorAll('.order__button');
-let buttonCloseModal = document.querySelector('.modal__close');
-
-buttonOrder.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        openOrder();
-    })
-});
-
-buttonCloseModal.addEventListener('click', () => {
-    closeModal()
-});
-
-const openOrder = () => {
-    modal.classList.add('modal_active')
-}
-
-const closeModal = () => {
-    modal.classList.remove('modal_active');
-};
-
-modal.addEventListener('click', closeModal);
-for( let ModalWindow of modal.children) {
-    ModalWindow.addEventListener('click', event=> {
-        event.stopPropagation();
-    })
-};
+// let modal = document.querySelector('.modal');
+// let buttonOrder = document.querySelectorAll('.order__button');
+// let buttonCloseModal = document.querySelector('.modal__close');
+//
+// buttonOrder.forEach((item, index) => {
+//     item.addEventListener('click', () => {
+//         openOrder();
+//     })
+// });
+//
+// buttonCloseModal.addEventListener('click', () => {
+//     closeModal()
+// });
+//
+// const openOrder = () => {
+//     modal.classList.add('modal_active')
+// }
+//
+// const closeModal = () => {
+//     modal.classList.remove('modal_active');
+// };
+//
+// modal.addEventListener('click', closeModal);
+// for( let ModalWindow of modal.children) {
+//     ModalWindow.addEventListener('click', event=> {
+//         event.stopPropagation();
+//     })
+// };
